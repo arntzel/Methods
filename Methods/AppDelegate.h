fbb0fcf0b442e774da9432c26676298d72c0abcd
Eliot <arntzel@gmail.com>
2013-03-02 10:09:02 -0500

    Initial Commit

---
+++ Methods/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Methods
+//
+//  Created by Eliot Arntz on 3/2/13.
+//  Copyright (c) 2013 EliotArntz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
